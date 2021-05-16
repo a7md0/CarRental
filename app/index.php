2d71b2727af5db74998003d9029e07ad63bddc1d
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-17 00:16:03 +0300

    Add boostrap 5

--- app/index.php
+++ app/index.php
@@ -7,10 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-
+    <div class="container">
+        <!-- Content here -->
+    </div>
 </body>
 
 </html>
