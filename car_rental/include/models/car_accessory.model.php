2913b0d5dc2a5d41adf5d3b5de63dad361f5667b
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-18 13:18:35 +0300

    Create car_accessory.model.php

---
+++ car_rental/include/models/car_accessory.model.php
@@ -0,0 +1,141 @@
+<?php
+require_once('model.class.php');
+
+class CarAccessory extends Model
+{
+    protected static $tableName = DB_TABLES_PREFIX . 'car_accessory';
+    protected static $primaryKeys = ['car_accessory_id'];
+    protected static $properties = ['car_accessory_id', 'car_type_id', 'name', 'preview_image', 'charge'];
+
+    /**
+     * Undocumented function
+     *
+     * @param array $data
+     * @return self
+     */
+    static function createFromDb(array $data)
+    {
+        $model = new self;
+
+        foreach ($data as $key => $value) {
+            $model->values[$key] = $value;
+        }
+
+        return $model;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param int $value
+     * @return self
+     */
+    function setCarAccessoryId($value)
+    {
+        parent::setValue('car_accessory_id', $value);
+
+        return $this;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return int
+     */
+    function getCarAccessoryId()
+    {
+        return parent::getValue('car_accessory_id');
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param int $value
+     * @return self
+     */
+    function setCarTypeId($value)
+    {
+        parent::setValue('car_type_id', $value);
+
+        return $this;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return int
+     */
+    function getCarTypeId()
+    {
+        return parent::getValue('car_type_id');
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param string $value
+     * @return self
+     */
+    function setName($value)
+    {
+        parent::setValue('name', $value);
+
+        return $this;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return string
+     */
+    function getName()
+    {
+        return parent::getValue('name');
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param string $value
+     * @return self
+     */
+    function setPreviewImage($value)
+    {
+        parent::setValue('preview_image', $value);
+
+        return $this;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return string
+     */
+    function getPreviewImage()
+    {
+        return parent::getValue('preview_image');
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param float $value
+     * @return self
+     */
+    function setCharge($value)
+    {
+        parent::setValue('charge', $value);
+
+        return $this;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return float
+     */
+    function getCharge()
+    {
+        return parent::getValue('charge');
+    }
+}
