72ee0e8231512e85502d78b2d3b7f7ca45508f29
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-18 13:18:38 +0300

    Update test.php

--- car_rental/test.php
+++ car_rental/test.php
@@ -3,6 +3,10 @@
 require_once('include/env.php');
 require_once('include/database.class.php');
 require_once('include/models/car.model.php');
+require_once('include/models/car_accessory.model.php');
 
 $car = Car::findOne(1);
 var_dump($car);
+
+$carAccessory = CarAccessory::findOne(100);
+var_dump($carAccessory);
