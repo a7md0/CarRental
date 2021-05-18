443f30cd114b7202c631bcb913ae9fb2ae62f34c
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-18 13:48:39 +0300

    Create car_reservation_accessory.model.php

---
+++ car_rental/include/models/car_reservation_accessory.model.php
@@ -0,0 +1,91 @@
+<?php
+require_once('model.class.php');
+
+class CarReservationAccessory extends Model
+{
+    protected static $tableName = DB_TABLES_PREFIX . 'car_reservation_accessory';
+    protected static $primaryKeys = ['car_id', 'user_car_reservation_id', 'car_accessory_id'];
+    protected static $properties = ['car_id', 'user_car_reservation_id', 'car_accessory_id'];
+
+    /*
+return parent::getValue('car_id');
+parent::setValue('car_id', $value);
+    */
+
+    /**
+     * Undocumented function
+     *
+     * @param array $data
+     * @return self
+     */
+    static function createFromDb(array $data)
+    {
+        $model = new self;
+
+        foreach ($data as $key => $value) {
+            $model->values[$key] = $value;
+        }
+
+        return $model;
+    }
+
+    /**
+     * Get the value of car_id
+     */
+    public function getCarId()
+    {
+        return parent::getValue('car_id');
+    }
+
+    /**
+     * Set the value of car_id
+     *
+     * @return  self
+     */
+    public function setCarId($value)
+    {
+        parent::setValue('car_id', $value);
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user_car_reservation_id
+     */
+    public function getUserCarReservationId()
+    {
+        return parent::getValue('user_car_reservation_id');
+    }
+
+    /**
+     * Set the value of user_car_reservation_id
+     *
+     * @return  self
+     */
+    public function setUserCarReservationId($value)
+    {
+        parent::setValue('user_car_reservation_id', $value);
+
+        return $this;
+    }
+
+    /**
+     * Get the value of car_accessory_id
+     */
+    public function getCarAccessoryId()
+    {
+        return parent::getValue('car_accessory_id');
+    }
+
+    /**
+     * Set the value of car_accessory_id
+     *
+     * @return  self
+     */
+    public function setCarAccessoryId($value)
+    {
+        parent::setValue('car_accessory_id', $value);
+
+        return $this;
+    }
+}
