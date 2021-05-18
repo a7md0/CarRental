86c95900f6a86675ca773ee52494944cd06dd727
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-18 14:11:48 +0300

    Create car_type.model.php

---
+++ car_rental/include/models/car_type.model.php
@@ -0,0 +1,71 @@
+<?php
+require_once('model.class.php');
+
+class CarType extends Model
+{
+    protected static $tableName = DB_TABLES_PREFIX . 'car_type';
+    protected static $primaryKeys = ['car_type_id'];
+    protected static $properties = ['car_type_id', 'type'];
+
+    /*
+return parent::getValue('car_id');
+parent::setValue('car_id', $value);
+    */
+
+    /**
+     * Undocumented function
+     *
+     * @param array $data
+     * @return self
+     */
+    static function createFromDb(array $data)
+    {
+        $model = new self;
+
+        foreach ($data as $key => $value) {
+            $model->values[$key] = $value;
+        }
+
+        return $model;
+    }
+
+    /**
+     * Get the value of car_type_id
+     */
+    public function getCarTypeId()
+    {
+        return parent::getValue('car_type_id');
+    }
+
+    /**
+     * Set the value of car_type_id
+     *
+     * @return  self
+     */
+    public function setCarTypeId($value)
+    {
+        parent::setValue('car_type_id', $value);
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     */
+    public function getType()
+    {
+        return parent::getValue('type');
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */
+    public function setType($value)
+    {
+        parent::setValue('type', $value);
+
+        return $this;
+    }
+}
