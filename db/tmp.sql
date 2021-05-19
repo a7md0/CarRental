553ea36096d285dda321052169849815e0f49af4
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-19 22:50:22 +0300

    Update tmp.sql

--- db/tmp.sql
+++ db/tmp.sql
@@ -11,3 +11,32 @@ INNER JOIN `dbproj_car_model` AS CM
 	ON C.`car_model_id` = CM.`car_model_id`
 
 WHERE CR.`car_id` IS NULL;
+
+-------------------------------------
+
+SELECT C.*, CM.*
+FROM `dbproj_car` AS C
+
+INNER JOIN `dbproj_car_model` AS CM
+	ON C.`car_model_id` = CM.`car_model_id`
+
+WHERE NOT EXISTS (SELECT DISTINCT UCR.`car_id` FROM `dbproj_user_car_reservation` AS UCR
+														WHERE UCR.`car_id` = C.`car_id`
+														AND UCR.`return_date` >= '2021-05-10' AND UCR.`pickup_date` <= '2021-05-22'
+									);
+									
+-------------------------------------
+					
+SELECT C.*, CM.*
+FROM `dbproj_car` AS C
+
+LEFT OUTER JOIN `dbproj_user_car_reservation` AS CR
+	ON C.`car_id` = CR.`car_id`
+    AND CR.`return_date` >= '2021-05-10' AND CR.`pickup_date` <= '2021-05-22'
+
+INNER JOIN `dbproj_car_model` AS CM
+	ON C.`car_model_id` = CM.`car_model_id`
+	
+WHERE CR.`car_id` IS NULL;
+
+----------------------------------------
