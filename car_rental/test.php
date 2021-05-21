b5050b86ecc562576d5c3a8932551bd4ea2dabc2
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-21 13:26:55 +0300

    Update test.php

--- car_rental/test.php
+++ car_rental/test.php
@@ -26,5 +26,5 @@ var_dump($userModel);
 $where0 = (new WhereClause())->where("x", "123", "=")->whereBetween("y", 1, 5);
 
 echo $where0->getSQL('ON') . "<br />";
-print_r($where0->types) . "<br />";
-print_r($where0->values) . "<br />";
+echo $where0->getTypes() . "<br />";
+print_r($where0->getValues()) . "<br />";
