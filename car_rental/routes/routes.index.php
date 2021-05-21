71d71c88fee7a52705e8e905c20e02d153a982e9
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-21 12:41:56 +0300

    Rename signin page

--- car_rental/routes/routes.index.php
+++ car_rental/routes/routes.index.php
@@ -2,7 +2,7 @@
 
 $ROUTES = [
     'home' => new Route('Home', 'home'),
-    'sign-in' => new UnauthorizedOnlyRoute('Sign-in', 'sign_in'),
+    'sign-in' => new UnauthorizedOnlyRoute('Sign-in', 'signin'),
     'signup' => new UnauthorizedOnlyRoute('Signup', 'signup'),
     'lookup-cars' => new Route('Lookup cars', 'lookup_cars'),
     'place-reservation' => new AuthorizedOnlyRoute('Place reservation', 'place_reservation'),
