a6c837a9fc4e67aefaec8a6945af56ab439400a2
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-21 18:15:44 +0300

    Update test.php

--- car_rental/test.php
+++ car_rental/test.php
@@ -1,37 +1,39 @@
 <?php
 
-require_once 'include/env.php';
-require_once 'include/database.class.php';
+require_once('include/env.php');
+require_once('include/database.class.php');
 
-require_once 'include/models/car.model.php';
-require_once 'include/models/car_accessory.model.php';
-require_once 'include/models/car_model.model.php';
+require_once('include/models/car.model.php');
+require_once('include/models/car_accessory.model.php');
+require_once('include/models/car_model.model.php');
 
-require_once 'include/models/user.model.php';
+require_once('include/models/user.model.php');
 
-require_once 'include/query/where_clause.class.php';
-require_once 'include/query/set_clause.class.php';
-require_once 'include/query/insert_clause.class.php';
+require_once('include/query/where_clause.class.php');
+require_once('include/query/set_clause.class.php');
+require_once('include/query/insert_clause.class.php');
 
 $car = Car::findById(1);
 var_dump($car);
 
-var_dump($car->insert());
+// var_dump($car->insert());
+// var_dump($car->delete());
 
-// $carAccessory = CarAccessory::findById(100);
-// var_dump($carAccessory);
 
-// $carModel = CarModel::findById(1000);
-// var_dump($carModel);
+$carAccessory = CarAccessory::findById(100);
+var_dump($carAccessory);
 
-// $userModel = User::findById(1);
-// var_dump($userModel);
+$carModel = CarModel::findById(1000);
+var_dump($carModel);
 
-// $cars = Car::find();
-// var_dump($cars);
+$userModel = User::findById(1);
+var_dump($userModel);
 
-// $cars = Car::count();
-// var_dump($cars);
+$cars = Car::find();
+var_dump($cars);
+
+$cars = Car::count();
+var_dump($cars);
 
 
 // $where0 = (new WhereClause())->where("x", "123", "=")->whereBetween("y", 1, 5);
