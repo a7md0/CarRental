a4489f086be802b251d98e1ce72149148d579ce2
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-21 17:07:57 +0300

    Update test.php

--- car_rental/test.php
+++ car_rental/test.php
@@ -10,28 +10,38 @@ require_once('include/models/car_model.model.php');
 require_once('include/models/user.model.php');
 
 require_once('include/query/where_clause.class.php');
+require_once('include/query/set_clause.class.php');
 
 $car = Car::findById(1);
 var_dump($car);
 
-$carAccessory = CarAccessory::findById(100);
-var_dump($carAccessory);
+$car->setValue('color', 'PINK');
 
-$carModel = CarModel::findById(1000);
-var_dump($carModel);
+var_dump($car->update());
 
-$userModel = User::findById(1);
-var_dump($userModel);
+// $carAccessory = CarAccessory::findById(100);
+// var_dump($carAccessory);
 
-$cars = Car::find();
-var_dump($cars);
+// $carModel = CarModel::findById(1000);
+// var_dump($carModel);
 
-$cars = Car::count();
-var_dump($cars);
+// $userModel = User::findById(1);
+// var_dump($userModel);
+
+// $cars = Car::find();
+// var_dump($cars);
+
+// $cars = Car::count();
+// var_dump($cars);
 
 
-$where0 = (new WhereClause())->where("x", "123", "=")->whereBetween("y", 1, 5);
+// $where0 = (new WhereClause())->where("x", "123", "=")->whereBetween("y", 1, 5);
 
-echo $where0->getSQL('ON') . "<br />";
-echo $where0->getTypes() . "<br />";
-print_r($where0->getValues()) . "<br />";
+// echo $where0->getSQL('ON') . "<br />";
+// echo $where0->getTypes() . "<br />";
+// print_r($where0->getValues()) . "<br />";
+
+// $set1 = new SetClause($car->values);
+// echo $set1->getSQL() . "<br />";
+// echo $set1->getTypes() . "<br />";
+// print_r($set1->getValues()) . "<br />";
