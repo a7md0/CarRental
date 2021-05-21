2a9b54cb2c422108690e1c9cae1e8cc3e3002821
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-21 12:21:51 +0300

    Update index.php

--- car_rental/index.php
+++ car_rental/index.php
@@ -11,26 +11,26 @@ require_once('routes/routes.php');
     <base href="/car_rental/">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$CURRENT_ROUTE->pageTitle?> - CRS</title>
+    <title><?= $CURRENT_ROUTE->pageTitle ?> - CRS</title>
 
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
-      }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
     </style>
 
-    
+
     <!-- Custom styles for this template -->
     <link href="assets/bootstrap/css/carousel.css" rel="stylesheet">
 
@@ -66,10 +66,13 @@ require_once('routes/routes.php');
         </nav>
     </header>
 
+    <?php $CURRENT_ROUTE->includePage(); ?>
+
     <main>
         <?php $CURRENT_ROUTE->includePage(); ?>
 
-        <!-- FOOTER -->
+        <hr class="featurette-divider">
+
         <footer class="container">
             <p class="float-end"><a href="#">Back to top</a></p>
             <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
