793faf6973ffa27017c68a09074ea6ec0c706529
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-21 20:18:25 +0300

    Sign-in add year

--- car_rental/pages/signin.page.php
+++ car_rental/pages/signin.page.php
@@ -34,4 +34,6 @@
         </label>
     </div>
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">&copy; <?= date("Y"); ?>
+</p>
 </form>
