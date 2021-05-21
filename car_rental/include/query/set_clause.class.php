5d19a53466c76a1b09174da40fb0897b243cc03e
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-21 16:55:25 +0300

    Create set_clause.class.php

---
+++ car_rental/include/query/set_clause.class.php
@@ -0,0 +1,74 @@
+<?php
+
+class SetClause
+{
+    /** @var Predicate[] */
+    private $predicates = [];
+    private $values = [];
+    private $types = [];
+
+    public function __construct(array $data)
+    {
+        foreach ($data as $column => $value) {
+            $val = $value;
+
+            if ($val == null) {
+                $val = 'NULL';
+            }
+
+            $this->predicates[] = "`$column` = ?";
+            $this->values[] = $val;
+            $this->types[] = static::typeOfValue($val);
+        }
+
+    }
+
+    public function hasAny()
+    {
+        return count($this->predicates) > 0;
+    }
+
+    /**
+     * Return built string part of the where clause.
+     *
+     * @return string
+     */
+    public function getSQL()
+    {
+        return 'SET ' . join(', ', $this->predicates);
+    }
+
+    /**
+     * Return string representation of current types in one string.
+     *
+     * @return string
+     */
+    public function getTypes()
+    {
+        return join('', $this->types);
+    }
+
+    /**
+     * Return values of the current where clause.
+     *
+     * @return array
+     */
+    public function getValues()
+    {
+        return $this->values;
+    }
+
+    private static function typeOfValue($value)
+    {
+        switch (gettype($value)) {
+            case 'integer':
+                return 'i';
+            case 'double':
+                return 'd';
+            case 'string':
+                return 's';
+            default:
+                return 'b';
+        }
+    }
+}
