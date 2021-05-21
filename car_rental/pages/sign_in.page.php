9230f60ca742d696fd172e79646d5d5d0069ba40
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-21 12:20:59 +0300

    Update sign_in.page.php

--- car_rental/pages/sign_in.page.php
+++ car_rental/pages/sign_in.page.php
@@ -34,6 +34,4 @@
         </label>
     </div>
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-    <?php include_once('pages/parts/footer.php'); ?>
 </form>
