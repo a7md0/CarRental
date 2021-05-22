f4840baf3b71633e9a36ac87eae2d5abd4add295
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-22 18:35:15 +0300

    Prepare signup page

---
+++ car_rental/pages/parts/signup.header.php
@@ -0,0 +1,52 @@
+<?php
+
+$CUSTOM_CLASSES['body'][] = 'text-center';
+$CUSTOM_CLASSES['main'][] = 'form-signup';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $CURRENT_USER == null) {
+
+}
+
+?>
+
+<style>
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        display: flex;
+        align-items: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+    }
+
+    .form-signup {
+        width: 100%;
+        max-width: 40em;
+        padding: 15px;
+        margin: auto;
+    }
+
+    .form-signup .checkbox {
+        font-weight: 400;
+    }
+
+    .form-signup .form-floating:focus-within {
+        z-index: 2;
+    }
+
+    .form-signup input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+
+    .form-signup input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+    }
+</style>
