a1df2c405b28d14ca6f3704a9eedd889b05fbba2
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-23 00:09:58 +0300

    Update test.php

--- car_rental/test.php
+++ car_rental/test.php
@@ -1,17 +1,8 @@
 <?php
 
 require_once('include/env.php');
-require_once('include/database.class.php');
+require_once('include/auto_loader.php');
 
-require_once('include/models/car.model.php');
-require_once('include/models/car_accessory.model.php');
-require_once('include/models/car_model.model.php');
-
-require_once('include/models/user.model.php');
-
-require_once('include/query/where_clause.class.php');
-require_once('include/query/set_clause.class.php');
-require_once('include/query/insert_clause.class.php');
 
 $car = Car::findById(1);
 var_dump($car);
@@ -29,18 +20,18 @@ var_dump($carModel);
 $userModel = User::findById(1);
 var_dump($userModel);
 
-$cars = Car::find();
-var_dump($cars);
+// $cars = Car::find();
+// var_dump($cars);
 
 $cars = Car::count();
 var_dump($cars);
 
 
-// $where0 = (new WhereClause())->where("x", "123", "=")->whereBetween("y", 1, 5);
+$where0 = (new WhereClause("C"))->where("x", "123", "=")->whereBetween("y", 1, 5)->whereFullText(['col1', 'col2'], 'query str');
 
-// echo $where0->getSQL('ON') . "<br />";
-// echo $where0->getTypes() . "<br />";
-// print_r($where0->getValues()) . "<br />";
+echo $where0->getSQL('ON') . "<br />";
+echo $where0->getTypes() . "<br />";
+print_r($where0->getValues()) . "<br />";
 
 // $set1 = new SetClause($car->values);
 // echo $set1->getSQL() . "<br />";
