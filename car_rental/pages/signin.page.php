d1461db18bfebb8ffcddcd7078aa75be0903555f
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-22 08:55:00 +0300

    Sign-in remove remember me

--- car_rental/pages/signin.page.php
+++ car_rental/pages/signin.page.php
@@ -28,11 +28,6 @@
         <label for="floatingPassword">Password</label>
     </div>
 
-    <div class="checkbox mb-3">
-        <label>
-            <input type="checkbox" value="remember-me"> Remember me
-        </label>
-    </div>
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
     <p class="mt-5 mb-3 text-muted">&copy; <?= date("Y"); ?>
 </p>
