4b10d2539045b9adba7fc309255a4b632026b8e5
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-22 20:33:05 +0300

    Home nav show active cond

--- car_rental/index.php
+++ car_rental/index.php
@@ -46,7 +46,7 @@ require_once 'routes/routes.php';
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link<?= $requestPage == 'home' ? ' active' : ''; ?>" aria-current="page" href="#">Home</a>
+                            <a class="nav-link<?= $requestPage == 'home' ? ' active' : ''; ?>" href="">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Link</a>
