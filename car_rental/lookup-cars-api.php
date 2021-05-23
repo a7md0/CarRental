38cd39436b8212779fb95c1249a7cd6d0437fc6b
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-23 22:44:32 +0300

    Create lookup-cars-api.php

---
+++ car_rental/lookup-cars-api.php
@@ -0,0 +1,54 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once 'include/env.php';
+    require_once 'include/auto_loader.php';
+
+    $json = file_get_contents('php://input');
+    $filters = json_decode($json);
+
+    header('Content-Type: application/json');
+
+    $data = [
+        'content' => '<b></b>',
+        'matching_results' => 0,
+        'pages' => [],
+        'query' => $filters->filter_pickup_date
+    ];
+
+    $carsLookup = new AdvanceCarsLookup($filters->filter_pickup_date, $filters->filter_return_date);
+
+    if (isset($filters->filter_search) && strlen($filters->filter_search) >= 4) {
+        $carsLookup->carModelWhereClause()->whereFullText(['brand', 'model'], $filters->filter_search);
+    }
+
+    if (isset($filters->filter_brands) && is_array($filters->filter_brands)) {
+        $carsLookup->carModelWhereClause()->whereIn('brand', $filters->filter_brands);
+    }
+
+    if (isset($filters->filter_types) && is_array($filters->filter_types)) {
+        $carsLookup->carModelWhereClause()->whereIn('car_type_id', $filters->filter_types);
+    }
+
+    if (isset($filters->filter_min_year) && isset($filters->filter_max_year)) {
+        $carsLookup->carModelWhereClause()->whereBetween('year', $filters->filter_min_year, $filters->filter_max_year);
+    }
+
+    if (isset($filters->filter_min_seats) && isset($filters->filter_max_seats)) {
+        $carsLookup->carModelWhereClause()->whereBetween('number_of_seats', $filters->filter_min_seats, $filters->filter_max_seats);
+    }
+
+    if (isset($filters->filter_min_price) && isset($filters->filter_max_price)) {
+        $carsLookup->carWhereClause()->whereBetween('daily_rent_rate', $filters->filter_min_price, $filters->filter_max_price);
+    }
+
+    $data['matching_results'] = $carsLookup->count();
+    if ($data['matching_results'] > 0) {
+        $cars = $carsLookup->find();
+    }
+
+    $data['query'] = $carsLookup->query;
+
+    exit(json_encode($data));
+}
+?>
