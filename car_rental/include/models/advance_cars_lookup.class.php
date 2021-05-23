ad6a0745df0b822e0332f6e036f5b0e71acf08d6
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-23 11:29:53 +0300

    Create advance_cars_lookup.class.php

---
+++ car_rental/include/models/advance_cars_lookup.class.php
@@ -0,0 +1,143 @@
+<?php
+
+class AdvanceCarsLookup
+{
+    /** @var WhereClause */
+    private $whereCarModel;
+    /** @var WhereClause */
+    private $whereCar;
+
+    private $pickupDate;
+    private $returnDate;
+
+    private $whereTypes = '';
+    private $whereValues = [];
+    private $query = '';
+
+    function __construct($pickupDate, $returnDate)
+    {
+        $this->whereCarModel = new WhereClause('CM');
+        $this->whereCar = new WhereClause('C');
+
+        $this->pickupDate = $pickupDate;
+        $this->returnDate = $returnDate;
+    }
+
+    private function buildQuery()
+    {
+        $this->whereTypes = '';
+        $this->whereValues = [];
+        $this->query = '';
+
+        $carTblName = Car::getTableName();
+        $userCarReservationTblName = UserCarReservation::getTableName();
+        $carModelTblName = CarModel::getTableName();
+
+        $onUserCarReservation = new WhereClause('UCR');
+        $onUserCarReservation->whereColumn('car_id', $this->whereCar->getColumnPrefix())
+            ->where('status', 'confirmed')
+            ->where('return_date', $this->pickupDate, '>=')
+            ->where('pickup_date', $this->returnDate, '<=');
+
+        $onUserCarReservationClause = $onUserCarReservation->getSQL('ON');
+        $this->whereTypes .= $onUserCarReservation->getTypes();
+        $this->whereValues += $onUserCarReservation->getValues();
+
+        $onCarModelClause = '';
+        if ($this->whereCarModel->hasAny()) {
+            $onCarModelClause = ' ' . $this->whereCarModel->getSQL('AND');
+            $this->whereTypes .= $this->whereCarModel->getTypes();
+            $this->whereValues += $this->whereCarModel->getValues();
+        }
+
+        $whereCarClause = '';
+        if ($this->whereCar->hasAny()) {
+            $whereCarClause = ' ' . $this->whereCar->getSQL('AND');
+            $this->whereTypes .= $this->whereCar->getTypes();
+            $this->whereValues += $this->whereCar->getValues();
+        }
+
+        $this->query = "SELECT C.*, CM.*
+        FROM `$carTblName` AS C
+
+        LEFT JOIN `$userCarReservationTblName` AS UCR
+            $onUserCarReservationClause
+
+        INNER JOIN `$carModelTblName` AS CM
+            ON C.`car_model_id` = CM.`car_model_id`$onCarModelClause
+
+        WHERE C.`status` = 'available'
+            AND UCR.`car_id` IS NULL$whereCarClause;";
+    }
+
+    /**
+     * Get the value of whereCarModel
+     */
+    public function carModelWhereClause()
+    {
+        return $this->whereCarModel;
+    }
+
+    /**
+     * Get the value of whereCar
+     */
+    public function carWhereClause()
+    {
+        return $this->whereCar;
+    }
+
+    /**
+     * Count matching rows and return count.
+     *
+     * @return int
+     */
+    public function count()
+    {
+        $this->buildQuery();
+
+        $stmt = Database::executeStatement($this->query, $this->whereTypes, $this->whereValues);
+        $count = 0;
+
+        if ($result = $stmt->get_result()) {
+            $row = $result->fetch_row();
+
+            if ($row != null) {
+                $count = $row[0];
+            }
+        }
+
+        $stmt->free_result();
+        $stmt->close();
+
+        return $count;
+    }
+
+    /**
+     * Find any matching records with the provided condition(s).
+     *
+     * @return Car[]
+     */
+    public function find()
+    {
+        $this->buildQuery();
+
+        $stmt = Database::executeStatement($this->query, $this->whereTypes, $this->whereValues);
+        $models = [];
+
+        if ($result = $stmt->get_result()) {
+            while ($row = $result->fetch_assoc()) {
+                $car = Car::initializeFromData($row);
+                $carModel = CarModel::initializeFromData($row);
+
+                $car->setCarModel($carModel);
+
+                $models[] = $car;
+            }
+        }
+
+        $stmt->free_result();
+        $stmt->close();
+
+        return $models;
+    }
+}
