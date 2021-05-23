4a98add5a445ce070c6df826ca8876d99f573894
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-23 18:33:31 +0300

    Update lookup_cars.header.php

--- car_rental/pages/parts/lookup_cars.header.php
+++ car_rental/pages/parts/lookup_cars.header.php
@@ -5,20 +5,34 @@
     $availableColors = Car::uniqueValues('color');
     $availableBrands = CarModel::uniqueValues('brand');
     $availableModels = CarModel::uniqueValues('model');
+    $availableTypes = CarType::find();
 
     list($minYear, $minSeats, $maxYear, $maxSeats) = CarModel::aggregateValues(['MIN' => ['year', 'number_of_seats'], 'MAX' => ['year', 'number_of_seats']]);
     list($minDailyRentRate, $maxDailyRentRate) = Car::aggregateValues(['MIN' => 'daily_rent_rate', 'MAX' => 'daily_rent_rate']);
 
-    var_dump($availableColors);
-    var_dump($availableBrands);
-    var_dump($availableModels);
+    // var_dump($availableColors);
+    // var_dump($availableBrands);
+    // var_dump($availableModels);
 
-    echo $minYear . '-' . $maxYear . '<br />';
-    echo $minDailyRentRate . '-' . $maxDailyRentRate . '<br />';
-    echo $minSeats . '-' . $maxSeats . '<br />';
+    // echo $minYear . '-' . $maxYear . '<br />';
+    // echo $minDailyRentRate . '-' . $maxDailyRentRate . '<br />';
+    // echo $minSeats . '-' . $maxSeats . '<br />';
 
-    $carsLookup = new AdvanceCarsLookup($pickupDate, $returnDate);
-    $cars = $carsLookup->find();
+    // $carsLookup = new AdvanceCarsLookup($pickupDate, $returnDate);
+    // $cars = $carsLookup->find();
+
+    $VALUES += [
+        'availableColors' => $availableColors,
+        'availableBrands' => $availableBrands,
+        'availableModels' => $availableModels,
+        'availableTypes' => $availableTypes,
+        'minYear' => $minYear,
+        'minSeats' => $minSeats,
+        'maxYear' => $maxYear,
+        'maxSeats' => $maxSeats,
+        'minDailyRentRate' => $minDailyRentRate,
+        'maxDailyRentRate' => $maxDailyRentRate,
+    ];
 ?>
 
 <script>
