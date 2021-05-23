1d09aca2d76b0d6f8b2d122707f02400df96d50e
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-23 23:45:10 +0300

    Seprate lookup_cars js

--- car_rental/pages/parts/lookup_cars.header.php
+++ car_rental/pages/parts/lookup_cars.header.php
@@ -37,7 +37,7 @@ $VALUES += [
 
 <script>
     (() => {
-        const ranges = {
+        window.lookup_cars_ranges =  {
             'year': {
                 'min': <?= $minYear; ?>,
                 'max': <?= $maxYear; ?>
@@ -51,110 +51,6 @@ $VALUES += [
                 'max': <?= $maxDailyRentRate; ?>
             },
         };
-
-        function checkRange(element) {
-            const filterMinElm = document.querySelector(`[name=filter_min_${element}]`);
-            const filterMaxElm = document.querySelector(`[name=filter_max_${element}]`);
-
-            filterMinElm.min = ranges[element].min;
-            filterMaxElm.max = ranges[element].max;
-
-            if (Number(filterMinElm.value) > Number(filterMaxElm.value)) {
-                if (Number(filterMinElm.value) > Number(filterMaxElm.max)) {
-                    filterMinElm.value = filterMinElm.min;
-                    filterMaxElm.value = filterMaxElm.max;
-                } else {
-                    filterMaxElm.value = filterMinElm.value;
-                }
-            }
-        }
-
-        function checkReservationDate() {
-            const filterPickupDateElm = document.querySelector('[name=filter_pickup_date]');
-            const filterReturnDateElm = document.querySelector('[name=filter_return_date]');
-
-            if (filterPickupDateElm.valueAsDate === null) {
-                filterPickupDateElm.valueAsDate = new Date();
-            }
-            if (filterReturnDateElm.valueAsDate === null) {
-                filterReturnDateElm.valueAsDate = new Date();
-            }
-
-            filterPickupDateElm.min = new Date().toISOString().split("T")[0];
-            filterReturnDateElm.min = filterPickupDateElm.min;
-
-            if (filterPickupDateElm.valueAsDate > filterReturnDateElm.valueAsDate) {
-                filterReturnDateElm.valueAsDate = filterPickupDateElm.valueAsDate;
-            }
-
-            const days = (filterReturnDateElm.valueAsDate - filterPickupDateElm.valueAsDate)  / (1000 * 3600 * 24) + 1;
-            document.querySelector('#reservation-period-in-days').innerText = ` (${days} days)`;
-        }
-
-        function fetchResults(filters) {
-            console.log(filters);
-
-            fetch('lookup-cars-api.php', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(filters)
-            }).then(rawResponse => rawResponse.json())
-            .then(response => {
-                console.log(response);
-                const resultsElement = document.querySelector('#results');
-                resultsElement.innerHTML = response.content;
-            });
-        }
-
-        function onFilterChange(event) {
-            const filterFormElement = document.querySelector('[name=filter_form]');
-
-            const filtersValid = filterFormElement.checkValidity();
-            if (!filtersValid) {
-                filterFormElement.reportValidity();
-                return;
-            }
-
-            checkReservationDate();
-            checkRange('price');
-            checkRange('year');
-            checkRange('seats');
-
-            // filtersValid = filterFormElement.checkValidity();
-            // if (!filtersValid) {
-            //     filterFormElement.reportValidity();
-            //     return;
-            // }
-
-            const filterElements = document.querySelectorAll('[data-trigger-filter=true]');
-            const data = {};
-            filterElements.forEach(element => {
-                // data[element.name]
-                const name = element.name;
-                let value = element.value;
-
-                if (element.nodeName === 'SELECT') {
-                    value = [...element.options].filter((x) => x.selected).map((x) => x.value);
-                }
-
-                data[name] = value;
-            });
-
-            fetchResults(data);
-        }
-
-        document.addEventListener('DOMContentLoaded', (event) => {
-            checkReservationDate();
-
-            const filterElements = document.querySelectorAll('[data-trigger-filter=true]');
-            filterElements.forEach(element => {
-                element.addEventListener('change', onFilterChange);
-            });
-
-            onFilterChange(null);
-        }, false);
     })();
 </script>
+<script src="assets/js/lookup_cars.js"></script>
