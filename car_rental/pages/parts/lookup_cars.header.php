213ccb45b691554770d5240f40df06e937dd34e1
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-23 17:49:11 +0300

    Update lookup_cars.header.php

--- car_rental/pages/parts/lookup_cars.header.php
+++ car_rental/pages/parts/lookup_cars.header.php
@@ -1,3 +1,26 @@
+<?php
+    $pickupDate = @$_GET['pickup_date'];
+    $returnDate = @$_GET['return_date'];
+
+    $availableColors = Car::uniqueValues('color');
+    $availableBrands = CarModel::uniqueValues('brand');
+    $availableModels = CarModel::uniqueValues('model');
+
+    list($minYear, $minSeats, $maxYear, $maxSeats) = CarModel::aggregateValues(['MIN' => ['year', 'number_of_seats'], 'MAX' => ['year', 'number_of_seats']]);
+    list($minDailyRentRate, $maxDailyRentRate) = Car::aggregateValues(['MIN' => 'daily_rent_rate', 'MAX' => 'daily_rent_rate']);
+
+    var_dump($availableColors);
+    var_dump($availableBrands);
+    var_dump($availableModels);
+
+    echo $minYear . '-' . $maxYear . '<br />';
+    echo $minDailyRentRate . '-' . $maxDailyRentRate . '<br />';
+    echo $minSeats . '-' . $maxSeats . '<br />';
+
+    $carsLookup = new AdvanceCarsLookup($pickupDate, $returnDate);
+    $cars = $carsLookup->find();
+?>
+
 <script>
     document.addEventListener('DOMContentLoaded', (event) => {
 
