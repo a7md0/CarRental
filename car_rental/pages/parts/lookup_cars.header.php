d976a2596669d9c6944b31e87ad12f11124dd361
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-24 17:09:57 +0300

    Update lookup_cars.header.php

--- car_rental/pages/parts/lookup_cars.header.php
+++ car_rental/pages/parts/lookup_cars.header.php
@@ -55,6 +55,16 @@ $VALUES += [
 </script>
 <script src="assets/js/lookup_cars.js"></script>
 <style>
+a.page-link {
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+}
+
 a.page-link:hover {
     cursor: pointer;
 }
