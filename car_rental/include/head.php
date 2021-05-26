46c0125214d67d337557d3730dffd96ca43d6f87
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-26 19:25:29 +0300

    Enable debugging cond

--- car_rental/include/head.php
+++ car_rental/include/head.php
@@ -1,5 +1,12 @@
 <?php
 
 require_once 'env.php';
+
+if (ENABLE_APP_DEBUG === true) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
 require_once 'database.class.php';
 require_once 'authentication.php';
