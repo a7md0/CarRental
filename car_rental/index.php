a6550486b99c7954f5488f96bad34249adb09f4e
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-26 19:27:59 +0300

    Disable base href (Fixes #4)

--- car_rental/index.php
+++ car_rental/index.php
@@ -9,7 +9,7 @@ require_once 'routes/routes.php';
 
 <head>
     <meta charset="UTF-8">
-    <base href="/car_rental/">
+    <!-- <base href="/car_rental/"> -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $CURRENT_ROUTE->pageTitle ?> - CRS</title>
