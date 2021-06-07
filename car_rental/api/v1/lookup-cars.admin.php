d9d1dd96556335411b54823a530fa1ce7aeb014f
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-06-07 19:09:46 +0300

    Implement lookup cars api for admin

---
+++ car_rental/api/v1/lookup-cars.admin.php
@@ -0,0 +1,95 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once '../../include/env.php';
+    require_once '../../include/auto_loader.php';
+
+    require_once '../../include/authentication.php';
+
+    /**
+     * @param Car[] $car
+     * @return string
+     */
+    function generateCarsTemplate($cars)
+    {
+        $cards = '';
+
+        /** @var Car */
+        foreach ($cars as $car) {
+            $cards .= '<div class="card car-card" style="width: 18rem;">
+            <img src="' . $car->getPreviewImage() . '" loading="lazy" class="card-img-top" alt="' . $car->getCarModel()->getFullDisplayName() . ' Image">
+            <div class="card-body text-center">
+                <h5 class="card-title">' . $car->getCarModel()->getFullDisplayName() . '</h5>
+                <p class="card-subtitle">BD' . $car->getDailyRentRate() . ' | ' . $car->getColor() . ' </p>
+                <a href="javascript:void(0)" class="btn btn-primary reserve-car-btn" data-car-id="' . $car->getCarId() . '">Reserve</a>
+            </div>
+            </div>';
+        }
+
+        return $cards;
+    }
+
+    $json = file_get_contents('php://input');
+    $filters = json_decode($json);
+
+    header('Content-Type: application/json');
+
+    if ($CURRENT_USER == null || ($CURRENT_USER != null && $CURRENT_USER->getUserType()->getAccessLevel() < 1)) {
+        http_response_code($CURRENT_USER == null ? 401 : 403); // Response code to be either 401 or 403
+
+        exit(json_encode([
+            // nothing to return
+        ]));
+    }
+
+    $data = [
+        'content' => '<b>No results</b>',
+        'matching_results' => 0,
+        'pages' => [
+            'total' => 0,
+            'current' => $filters->currentPage ?? 1,
+        ]
+    ];
+
+    $carsLookup = new AdvancedCarsLookup($filters->filter_pickup_date, $filters->filter_return_date);
+    $carsLookup->setCurrentPage($filters->currentPage);
+
+    if (isset($filters->filter_search) && strlen($filters->filter_search) >= 4) {
+        $carsLookup->carModelWhereClause()->whereFullText(['brand', 'model'], $filters->filter_search);
+    }
+
+    if (isset($filters->filter_brands) && is_array($filters->filter_brands)) {
+        $carsLookup->carModelWhereClause()->whereIn('brand', $filters->filter_brands);
+    }
+
+    if (isset($filters->filter_types) && is_array($filters->filter_types)) {
+        $carsLookup->carModelWhereClause()->whereIn('car_type_id', $filters->filter_types);
+    }
+
+    if (isset($filters->filter_min_year) && isset($filters->filter_max_year)) {
+        $carsLookup->carModelWhereClause()->whereBetween('year', $filters->filter_min_year, $filters->filter_max_year);
+    }
+
+    if (isset($filters->filter_min_seats) && isset($filters->filter_max_seats)) {
+        $carsLookup->carModelWhereClause()->whereBetween('number_of_seats', $filters->filter_min_seats, $filters->filter_max_seats);
+    }
+
+    if (isset($filters->filter_min_price) && isset($filters->filter_max_price)) {
+        $carsLookup->carWhereClause()->whereBetween('daily_rent_rate', $filters->filter_min_price, $filters->filter_max_price);
+    }
+
+    if (isset($filters->filter_colors) && is_array($filters->filter_colors)) {
+        $carsLookup->carWhereClause()->whereIn('color', $filters->filter_colors);
+    }
+
+    $data['matching_results'] = $carsLookup->count();
+    if ($data['matching_results'] > 0) {
+        $cars = $carsLookup->find();
+        $data['content'] = generateCarsTemplate($cars);
+    }
+
+    $data['pages']['total'] = $carsLookup->getTotalPages();
+    // $data['query'] = $carsLookup->query;
+
+    exit(json_encode($data));
+}
---
+++ car_rental/include/custom/advanced_cars_lookup.class.php
@@ -0,0 +1,133 @@
+<?php
+
+class AdvancedCarsLookup
+{
+    use Pagination;
+
+    /** @var WhereClause */
+    private $whereCarModel;
+    /** @var WhereClause */
+    private $whereCar;
+
+    private $whereTypes = '';
+    private $whereValues = [];
+    public $query = '';
+
+    function __construct()
+    {
+        $this->whereCarModel = new WhereClause('CM');
+        $this->whereCar = new WhereClause('C');
+    }
+
+    private function buildQuery()
+    {
+        $this->whereTypes = '';
+        $this->whereValues = [];
+        $this->query = '';
+
+        $carTblName = Car::getTableName();
+        $carModelTblName = CarModel::getTableName();
+        $carTypeTblName = CarType::getTableName();
+
+        $onCarModelClause = '';
+        if ($this->whereCarModel->hasAny()) {
+            $onCarModelClause = ' ' . $this->whereCarModel->getSQL('AND');
+            $this->whereTypes .= $this->whereCarModel->getTypes();
+            $this->whereValues = array_merge($this->whereValues, $this->whereCarModel->getValues());
+        }
+
+        $whereCarClause = '';
+        if ($this->whereCar->hasAny()) {
+            $whereCarClause = $this->whereCar->getSQL();
+            $this->whereTypes .= $this->whereCar->getTypes();
+            $this->whereValues = array_merge($this->whereValues, $this->whereCar->getValues());
+        }
+
+        $this->query = " FROM `$carTblName` AS C
+
+        INNER JOIN `$carModelTblName` AS CM
+            ON C.`car_model_id` = CM.`car_model_id`$onCarModelClause
+
+        INNER JOIN `$carTypeTblName` AS CT
+            ON CM.`car_type_id` = CT.`car_type_id`
+
+        $whereCarClause";
+    }
+
+    /**
+     * Get the value of whereCarModel
+     */
+    public function carModelWhereClause()
+    {
+        return $this->whereCarModel;
+    }
+
+    /**
+     * Get the value of whereCar
+     */
+    public function carWhereClause()
+    {
+        return $this->whereCar;
+    }
+
+    /**
+     * Count matching rows and return count.
+     *
+     * @return int
+     */
+    public function count()
+    {
+        $this->buildQuery();
+        $queryPrefix = 'SELECT COUNT(*)';
+        $querySuffix = ';';
+
+        $stmt = Database::executeStatement($queryPrefix . $this->query . $querySuffix, $this->whereTypes, $this->whereValues);
+        $count = 0;
+
+        if ($result = $stmt->get_result()) {
+            $row = $result->fetch_row();
+
+            if ($row != null) {
+                $count = $row[0];
+            }
+        }
+
+        $stmt->free_result();
+        $stmt->close();
+
+        return $count;
+    }
+
+    /**
+     * Find any matching records with the provided condition(s).
+     *
+     * @return Car[]
+     */
+    public function find()
+    {
+        $this->buildQuery();
+        $queryPrefix = 'SELECT C.*, CM.*, CT.`type`';
+        $querySuffix = $this->getLimitClause() . ';';
+
+        $stmt = Database::executeStatement($queryPrefix . $this->query . $querySuffix, $this->whereTypes, $this->whereValues);
+        $models = [];
+
+        if ($result = $stmt->get_result()) {
+            while ($row = $result->fetch_assoc()) {
+                $car = Car::initializeFromData($row);
+                $carModel = CarModel::initializeFromData($row);
+                $carType = CarType::initializeFromData($row);
+
+                $car->setCarModel($carModel);
+                $car->setCarType($carType);
+
+                $models[] = $car;
+            }
+        }
+
+        $stmt->free_result();
+        $stmt->close();
+
+        return $models;
+    }
+}
