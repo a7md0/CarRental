86f5bd2509906eac6d1cc9621490255ce9a1618f
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-06-07 20:27:29 +0300

    Update routes.admin.php

--- car_rental/routes/routes.admin.php
+++ car_rental/routes/routes.admin.php
@@ -1,6 +1,7 @@
 <?php
 
 $ROUTES = [
-    'home' => new AdminOnlyRoute('Home', 'home', 'pages/admin'),
+    'home' => new AdminOnlyRoute('Home', 'home'),
+    'cars' => new AdminOnlyRoute('Cars', 'cars'),
     // 'sign-in' => new UnauthorizedOnlyRoute('Sign-in', 'sign_in', 'pages/admin'),
 ];
