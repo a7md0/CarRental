a7a3f7eab0382f9abd98ec2e152735e8b97c16a4
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-06-07 03:54:00 +0300

    Style error pages

--- car_rental/pages/errors/401.page.php
+++ car_rental/pages/errors/401.page.php
@@ -1,1 +1,8 @@
-Unauthorized (401)
+<div class="d-flex" style="height: 80vh;">
+    <div class="d-flex w-100 justify-content-center align-self-center align-items-center">
+        <h1 class="me-3 pe-3 align-top border-end inline-block align-content-center">401</h1>
+        <div class="inline-block align-middle">
+            <h2 class="font-weight-normal lead" id="desc">You are unauthorized to access this page, please consider signing in.</h2>
+        </div>
+    </div>
+</div>
--- car_rental/pages/errors/403.page.php
+++ car_rental/pages/errors/403.page.php
@@ -1,8 +1,8 @@
 <div class="d-flex" style="height: 80vh;">
     <div class="d-flex w-100 justify-content-center align-self-center align-items-center">
-        <h1 class="me-3 pe-3 align-top border-end inline-block align-content-center">404</h1>
+        <h1 class="me-3 pe-3 align-top border-end inline-block align-content-center">403</h1>
         <div class="inline-block align-middle">
-            <h2 class="font-weight-normal lead" id="desc">The page you requested was not found.</h2>
+            <h2 class="font-weight-normal lead" id="desc">You are forbidden from accessing this page!</h2>
         </div>
     </div>
 </div>
