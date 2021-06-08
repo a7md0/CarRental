7368f9932b37ef5d822987f4f22e54dc4a5242f7
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-06-09 00:59:04 +0300

    Update index.php

--- car_rental/index.php
+++ car_rental/index.php
@@ -46,7 +46,7 @@ require_once 'routes/routes.php';
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link<?= $requestPage == 'home' ? ' active' : ''; ?>" href="">Home</a>
+                            <a class="nav-link<?= $requestPage == 'home' ? ' active' : ''; ?>" href="?p=home">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link<?= $requestPage == 'lookup-cars' ? ' active' : ''; ?>" href="?p=lookup-cars">Lookup cars</a>
