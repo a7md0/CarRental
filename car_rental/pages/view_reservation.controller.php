29303061eb9714228a720ea79a4c6772feb17ab5
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-06-09 05:05:56 +0300

    View res implement

--- car_rental/pages/view_reservation.controller.php
+++ car_rental/pages/view_reservation.controller.php
@@ -1,38 +1,46 @@
 <?php
 
-$reservationCode = intval($_GET['reservationCode']);
-$source = isset($_GET['from']) ? $_GET['from'] : '';
+if (isset($_GET['reservationCode'])) {
+    $reservationCode = intval($_GET['reservationCode']);
+    $source = isset($_GET['from']) ? $_GET['from'] : '';
 
-$whereClause = new WhereClause();
-$whereClause->where('reservation_code', $reservationCode)
-    ->where('user_id', $CURRENT_USER->getUserId());
+    $whereClause = new WhereClause();
+    $whereClause->where('reservation_code', $reservationCode)
+        ->where('user_id', $CURRENT_USER->getUserId());
 
-$reservation = UserCarReservation::findOne($whereClause);
+    $reservation = UserCarReservation::findOne($whereClause);
 
-$successMessage = '';
+    $successMessage = '';
 
-if ($reservation != null) {
-    if ($source == 'checkout') {
-        $successMessage = "Your reservation have been confirmed successfully.";
+    if ($reservation != null) {
+        if ($source == 'checkout') {
+            $successMessage = "Your reservation have been confirmed successfully.";
+        }
+
+        $carDetails = CarDetail::findById($reservation->getCarId());
+
+        $salesInvoice = SalesInvoice::findById($reservation->getSalesInvoiceId());
+
+        $salesInvoiceItemWhereClause = (new WhereClause())->where('sales_invoice_id', $reservation->getSalesInvoiceId());
+        $salesInvoiceItems = SalesInvoiceItem::find($salesInvoiceItemWhereClause);
+
+        $accessoriesWhereClause = (new WhereClause())->where('user_car_reservation_id', $reservation->getUserCarReservationId());
+        $accessories = CarAccessory::findJoined('car_reservation_accessory', 'car_accessory_id', $accessoriesWhereClause);
+
+        $paidAmount = $salesInvoice->getPaidAmount();
+        $totalAmount = $salesInvoice->getGrandTotal();
+        $dueAmount = $totalAmount - $paidAmount;
     }
 
-    $carDetails = CarDetail::findById($reservation->getCarId());
-
-    $salesInvoice = SalesInvoice::findById($reservation->getSalesInvoiceId());
-
-    $salesInvoiceItemWhereClause = (new WhereClause())->where('sales_invoice_id', $reservation->getSalesInvoiceId());
-    $salesInvoiceItems = SalesInvoiceItem::find($salesInvoiceItemWhereClause);
-
-    $paidAmount = $salesInvoice->getPaidAmount();
-    $totalAmount = $salesInvoice->getGrandTotal() - $paidAmount;
+    $VALUES += [
+        'reservationCode' => $reservationCode,
+        'successMessage' => $successMessage,
+        'reservation' => $reservation,
+        'carDetails' => $carDetails,
+        'salesInvoice' => $salesInvoice,
+        'accessories' => $accessories,
+        'paidAmount' => $paidAmount,
+        'totalAmount' => $totalAmount,
+        'dueAmount' => $dueAmount,
+    ];
 }
-
-$VALUES += [
-    'successMessage' => $successMessage,
-    'reservation' => $reservation,
-    'carDetails' => $carDetails,
-    'salesInvoice' => $salesInvoice,
-    'salesInvoiceItems' => $salesInvoiceItems,
-    'paidAmount' => $paidAmount,
-    'totalAmount' => $totalAmount,
-];
--- car_rental/pages/view_reservation.view.php
+++ car_rental/pages/view_reservation.view.php
@@ -1,4 +1,15 @@
-<?= $_GET['reservationCode'] ?>
+<?php
+
+/**
+ * @var UserCarReservation $reservation
+ * @var CarDetail $carDetails
+ * @var CarAccessory[] $accessories
+ * @var float $paidAmount
+ * @var float $totalAmount
+ * @var float $dueAmount
+ * @var string $reservationCode
+ */
+?>
 <div class="container" style="max-width: 1080px;">
     <div class="py-5 text-center">
         <h2>View reservation</h2>
@@ -11,122 +22,143 @@
     <?php } ?>
     <div class="row g-5">
         <div class="col-md-12">
-            <h4 class="mb-3">Lookup reservation</h4>
             <div class="row">
                 <div class="col-md-4"></div>
-                <div class="col-md-4">
+                <div class="col-md-4 text-center">
+                    <label for="reservation-code" class="form-label">Reservation code</label>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Reservation code" value="<?= isset($_GET['reservationCode']) ? $_GET['reservationCode'] : '' ?>" aria-describedby="view-button">
-                        <button class="btn btn-secondary" type="button" id="view-button">View</button>
+                        <input type="text" id="reservation-code" class="form-control" placeholder="Reservation code" value="<?= isset($_GET['reservationCode']) ? $_GET['reservationCode'] : '' ?>" aria-describedby="view-button">
+                        <button class="btn btn-secondary" type="submit" id="view-button">Lookup</button>
                     </div>
                 </div>
                 <div class="col-md-4"></div>
             </div>
-            <h4 class="mb-3">Car details</h4>
 
-            <div class="row">
-                <div class="col-md-4">
-                    <img src="<?= $carDetails->getPreviewImage(); ?>" class="card-img-top" alt="...">
-                </div>
+            <?php if (isset($reservationCode)) { ?>
+            <div class="card">
+                <article class="card-group-item">
+                    <h5 class="card-header d-flex justify-content-between align-items-center">
+                        View reservation (#<?= $reservationCode ?>)
+                        <div>
+                            <button type="button" class="btn btn-sm btn-warning">Amend</button>
+                            <button type="button" class="btn btn-sm btn-danger">Cancel</button>
+                        </div>
+                    </h5>
 
-                <div class="col-md-1"></div>
 
-                <div class="col-md-7">
-                    <div class="row">
-                        <div class="col-md-7">
-                            <span class="fw-bold">Car</span><br />
-                            <span><?= $carDetails->getCarModel()->getFullDisplayName() ?></span>
-                        </div>
-                        <div class="col-md-5">
-                            <span class="fw-bold">Color</span><br />
-                            <span><?= $carDetails->getColor() ?></span>
-                        </div>
-                        <div class="col-md-7">
-                        <span class="fw-bold">Type</span><br />
-                            <span><?= $carDetails->getCarType()->getType() ?></span>
-                        </div>
-                        <div class="col-md-5">
-                            <span class="fw-bold">Daily rent rate</span><br />
-                            <span>BD<?= $carDetails->getDailyRentRate() ?></span>
-                        </div>
+                    <div class="card-body m-3">
+                        <?php
+                        if (isset($reservation)) { ?>
+                            <h4 class="mb-3">Reservation details</h4>
+
+                            <div class="row d-flex align-items-center text-center mb-4">
+                                <div class="col-md-4 mb-2">
+                                    <h6>Pickup date</h6>
+                                    <span><?= $reservation->getPickupDate() ?></span>
+                                </div>
+                                <div class="col-md-4 mb-2">
+                                    <h6>Return date</h6>
+                                    <span><?= $reservation->getReturnDate() ?></span>
+                                </div>
+                                <div class="col-md-4 mb-2">
+                                    <h6>Status</h6>
+                                    <span><?= $reservation->getStatus() ?></span>
+                                </div>
+
+                                <div class="col-md-4 mb-2">
+                                    <h6>Total amount</h6>
+                                    <span>BD<?= $totalAmount ?></span>
+                                </div>
+                                <div class="col-md-4 mb-2">
+                                    <h6>Paid amount</h6>
+                                    <span>BD<?= $paidAmount ?></span>
+                                </div>
+                                <div class="col-md-4 mb-2">
+                                    <h6>Due amount</h6>
+                                    <span>BD<?= $dueAmount ?></span>
+                                </div>
+                            </div>
+                        <?php } ?>
+
+                        <?php if (isset($carDetails)) { ?>
+                            <h4 class="mb-3">Car details</h4>
+
+                            <div class="row mb-4">
+                                <div class="col-md-4">
+                                    <img src="<?= $carDetails->getPreviewImage(); ?>" class="card-img-top" alt="...">
+                                </div>
+
+                                <!-- <div class="col-md-1"></div> -->
+
+                                <div class="col-md-8">
+                                    <div class="row align-items-center text-center mb-4">
+                                        <div class="col-md-6 mb-2">
+                                            <h6>Car</h6>
+                                            <span><?= $carDetails->getCarModel()->getFullDisplayName() ?></span>
+                                        </div>
+                                        <div class="col-md-6 mb-2">
+                                            <h6>Color</h6>
+                                            <span><?= $carDetails->getColor() ?></span>
+                                        </div>
+                                        <div class="col-md-6 mb-2">
+                                            <h6>Type</h6>
+                                            <span><?= $carDetails->getCarType()->getType() ?></span>
+                                        </div>
+                                        <div class="col-md-6 mb-2">
+                                            <h6>Daily rent rate</h6>
+                                            <span>BD<?= $carDetails->getDailyRentRate() ?></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <?php } ?>
+
+                        <?php if (isset($accessories)) { ?>
+                            <div class="col-md-12">
+                                <!-- <h5 class="mb-3">Accessories</h5> -->
+                                <h4 class="d-flex justify-content-between align-items-center mb-3">
+                                    <span>Accessories</span>
+                                    <span class="badge bg-secondary rounded-pill"><?= count($accessories); ?></span>
+                                </h4>
+
+                                <ul class="list-group mb-3">
+                                    <?php foreach ($accessories as $accessory) { ?>
+                                        <li class="list-group-item d-flex justify-content-between lh-sm">
+                                            <div class="p-4">
+                                                <img src="<?= $accessory->getPreviewImage(); ?>" class="card-img-top" style="object-fit: contain; max-width: 6em; max-height: 6em;">
+                                            </div>
+                                            <div class="p-2 flex-fill align-self-center">
+                                                <h6 class="my-0"><?= $accessory->getName(); ?></h6>
+                                                <!-- <small class="text-muted">Brief description</small> -->
+                                            </div>
+                                            <div class="p-2 align-self-center">
+                                                <span class="text-muted">BD<?= $accessory->getCharge(); ?></span>
+                                            </div>
+                                        </li>
+                                    <?php
+                                    }
+                                    ?>
+                                </ul>
+                            </div>
+                        <?php } ?>
+
                     </div>
-                </div>
+
+                </article>
             </div>
-
-            <form class="needs-validation" action="<?= $_SERVER['REQUEST_URI'] ?>" method="POST">
-                <div class="row g-3">
-                    <div class="col-12">
-                        <label for="address" class="form-label">Address</label>
-                        <input type="text" class="form-control" name="address1" placeholder="1234 Main St" autocomplete="address-line1" required>
-                        <div class="invalid-feedback">
-                            Please enter your shipping address.
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-                        <input type="text" class="form-control" name="address2" placeholder="Apartment or suite" autocomplete="address-line2">
-                    </div>
-
-                    <div class="col-md-5">
-                        <label for="country" class="form-label">Country</label>
-
-                        <div class="invalid-feedback">
-                            Please select a valid country.
-                        </div>
-                    </div>
-
-                    <div class="col-md-4">
-                        <label for="city" class="form-label">City <span class="text-muted">(Optional)</span></label>
-                        <input type="text" class="form-control" name="city" placeholder="" autocomplete="address-level2">
-                        <div class="invalid-feedback">
-                            City code required.
-                        </div>
-                    </div>
-
-                    <div class="col-md-3">
-                        <label for="zip" class="form-label">Zip <span class="text-muted">(Optional)</span></label>
-                        <input type="text" class="form-control" name="zip" placeholder="" autocomplete="postal-code">
-                        <div class="invalid-feedback">
-                            Zip code required.
-                        </div>
-                    </div>
-                </div>
-
-                <hr class="my-4">
-
-                <h4 class="mb-3">Payment</h4>
-
-                <div class="row gy-3">
-                    <div class="col-md-6">
-                        <label for="cc-number" class="form-label">Credit card number</label>
-                        <input type="text" class="form-control" name="cc_number" placeholder="1234 5678 9112 1314" minlength="16" maxlength="16" autocomplete="cc-number" required>
-                        <div class="invalid-feedback">
-                            Credit card number is required
-                        </div>
-                    </div>
-
-                    <div class="col-md-3">
-                        <label for="cc-expiration" class="form-label">Expiration</label>
-                        <input type="text" class="form-control" name="cc_expiration" placeholder="MM/YY" pattern="(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})" autocomplete="cc-exp" required>
-                        <div class="invalid-feedback">
-                            Expiration date required
-                        </div>
-                    </div>
-
-                    <div class="col-md-3">
-                        <label for="cc-cvv" class="form-label">CVV</label>
-                        <input type="text" class="form-control" name="cc_cvv" placeholder="000" minlength="3" maxlength="3" autocomplete="cc-csc" required>
-                        <div class="invalid-feedback">
-                            Security code required
-                        </div>
-                    </div>
-                </div>
-
-                <hr class="my-4">
-
-                <button class="w-100 btn btn-primary btn-lg" type="submit">Checkout</button>
-            </form>
+            <?php } ?>
         </div>
     </div>
 </div>
+
+<script>
+    const viewButton = document.querySelector("#view-button");
+    viewButton.addEventListener('click', (event) => {
+        const reservationCodeElement = document.querySelector("#reservation-code");
+
+        submit_get({
+            reservationCode: reservationCodeElement.value
+        });
+    });
+</script>
