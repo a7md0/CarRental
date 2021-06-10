095629600868b1466a1a741861b20753820fba97
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-06-10 08:32:46 +0300

    car_form implement add

--- car_rental/pages/admin/car_form.controller.php
+++ car_rental/pages/admin/car_form.controller.php
@@ -1,29 +1,35 @@
 <?php
 
 $carModels = CarModel::find();
-$isAdding = false;
+$isAdding = true;
+
+function handleUpload(Car $c)
+{
+    if (isset($_FILES['preview_image']) && $_FILES['preview_image']['size'] > 0) {
+        try {
+            $upload = new Upload('assets/images/upload/');
+            $file = $upload->upload($_FILES['preview_image']);
+
+            $c->setPreviewImage($file->getPath());
+        } catch (Exception $ex) {
+            // echo $ex->getMessage();
+            // TODO: Show upload image error
+        }
+    }
+}
 
 if (isset($_GET['carId'])) {
     $carId = intval($_GET['carId']);
     $car = Car::findById($carId);
 
     if ($car != null) {
+        $isAdding = false;
         $VALUES['car'] = $car;
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // TODO: Validate inputs
 
-            if (isset($_FILES['preview_image']) && $_FILES['preview_image']['size'] > 0) {
-                try {
-                    $upload = new Upload('assets/images/upload/');
-                    $file = $upload->upload($_FILES['preview_image']);
-
-                    $car->setPreviewImage($file->getPath());
-                } catch (Exception $ex) {
-                    // echo $ex->getMessage();
-                    // TODO: Show upload image error
-                }
-            }
+            handleUpload($car);
 
             $car->setCarModelId($_POST['car_model_id'])
                 ->setLicensePlate($_POST['license_plate'])
@@ -34,6 +40,22 @@ if (isset($_GET['carId'])) {
 
             $car->update();
         }
+    }
+} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $car = new Car();
+
+    handleUpload($car);
+
+    $car->setCarModelId($_POST['car_model_id'])
+        ->setLicensePlate($_POST['license_plate'])
+        ->setVehicleIdentificationNumber($_POST['vin'])
+        ->setColor($_POST['color'])
+        ->setDailyRentRate($_POST['daily_rent_fees'])
+        ->setStatus($_POST['status']);
+
+    if ($car->insert()) {
+        header("Location: ?p=car-form&carId={$car->getCarId()}");
+        exit;
     }
 }
 
