f9fa7801eeced0cb4d8ee9f1fe077f5e789313d8
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-06-12 17:28:36 +0300

    Admin home page implement

--- car_rental/pages/admin/home.view.php
+++ car_rental/pages/admin/home.view.php
@@ -1,43 +1,32 @@
-
-<!-- Marketing messaging and featurettes
-  ================================================== -->
-<!-- Wrap the rest of the page in another container to center all the content. -->
-
 <div class="container marketing mt-3">
 
-    <!-- Three columns of text below the carousel -->
-    <div class="row">
-        <div class="col-lg-4">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-            </svg>
-
-            <h2>Heading</h2>
-            <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-            </svg>
-
-            <h2>Heading</h2>
-            <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-            </svg>
-
-            <h2>Heading</h2>
-            <p>And lastly this, the third column of representative placeholder content.</p>
-            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-    </div><!-- /.row -->
+    <div class="d-flex" style="height: 80vh;">
+        <div class="d-flex w-100 justify-content-center align-self-center align-items-center">
+            <!-- Three columns of text below the carousel -->
+            <div class="row text-center">
+                <div class="col-lg-3">
+                    <h2>All cars</h2>
+                    <p>Lookup all cars in the system, or a specific ones using the filters tools.</p>
+                    <p><a class="btn btn-secondary" href="?p=cars">Cars &raquo;</a></p>
+                </div>
+                <div class="col-lg-3">
+                    <h2>Add new car</h2>
+                    <p>Add a new car to the system and make it available for rent.</p>
+                    <p><a class="btn btn-secondary" href="?p=car">Add car &raquo;</a></p>
+                </div>
+                <div class="col-lg-3">
+                    <h2>Most popular car</h2>
+                    <p>Generate a report of the most popular reserved cars.</p>
+                    <p><a class="btn btn-secondary" href="?p=popular-reserved-cars">View report &raquo;</a></p>
+                </div>
+                <div class="col-lg-3">
+                    <h2>Sales revenue</h2>
+                    <p>Generate report of the sales revenue for any given period time.</p>
+                    <p><a class="btn btn-secondary" href="?p=sales-revenue">View report &raquo;</a></p>
+                </div>
+            </div><!-- /.row -->
 
 
+        </div>
+    </div>
 </div><!-- /.container -->
