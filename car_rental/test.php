e3964a06ceaab545a3030cf97e5f9a4c4c73d483
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-06-14 03:01:42 +0300

    Update test.php
    Co-Authored-By: MrGhareeb <50526062+MrGhareeb@users.noreply.github.com>

--- car_rental/test.php
+++ car_rental/test.php
@@ -1,21 +1,25 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once('include/env.php');
 require_once('include/auto_loader.php');
 
 
-$car = Car::findById(1);
-var_dump($car);
+// $car = Car::findById(1);
+// var_dump($car);
 
 // var_dump($car->insert());
 // var_dump($car->delete());
 
 
-$carAccessory = CarAccessory::findById(100);
-var_dump($carAccessory);
+// $carAccessory = CarAccessory::findById(100);
+// var_dump($carAccessory);
 
-$carModel = CarModel::findById(1000);
-var_dump($carModel);
+// $carModel = CarModel::findById(1000);
+// var_dump($carModel);
 
 // $userModel = User::findById(1);
 // var_dump($userModel);
@@ -23,10 +27,10 @@ var_dump($carModel);
 // $cars = Car::find();
 // var_dump($cars);
 
-$cars = Car::count();
-var_dump($cars);
+// $cars = Car::count();
+// var_dump($cars);
 
-var_dump(CarModel::aggregateValues(['MIN' => ['year', 'number_of_seats'], 'MAX' => ['year', 'number_of_seats']]));
+// var_dump(CarModel::aggregateValues(['MIN' => ['year', 'number_of_seats'], 'MAX' => ['year', 'number_of_seats']]));
 
 // $reservation = UserCarReservation::findById(12);
 // $reservation->cancel();
@@ -43,18 +47,20 @@ var_dump(CarModel::aggregateValues(['MIN' => ['year', 'number_of_seats'], 'MAX' 
 // echo $set1->getTypes() . "<br />";
 // print_r($set1->getValues()) . "<br />";
 
-$car2 = CarDetail::findById(1);
-var_dump($car2);
+// $car2 = CarDetail::findById(1);
+// var_dump($car2);
 
 
-$acs = CarAccessory::findJoined('car_reservation_accessory', 'car_accessory_id', (new WhereClause())->where('user_car_reservation_id', 10));
-var_dump($acs);
+// $acs = CarAccessory::findJoined('car_reservation_accessory', 'car_accessory_id', (new WhereClause())->where('user_car_reservation_id', 10));
+// var_dump($acs);
 
 
-$res = UserCarReservation::findById(15);
-$can = $res->canAmend($why);
-var_dump($can);
-echo "$why";
-var_dump($res->canCancel());
-$res->amend('2021-06-15', '2021-06-17', $err);
+$res = UserCarReservation::findById(26);
+$res->setStatus('confirmed');
+var_dump($res->update());
+// $can = $res->canAmend($why);
+// var_dump($can);
+// echo "$why";
+// // var_dump($res->canCancel());
+// $res->amend('2021-07-15', '2021-07-17', $err);
 
